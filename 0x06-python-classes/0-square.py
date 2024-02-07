15e0779c50549f4e304776c046a4a687c6d44d99
jan_ice17 <judysjewel17@gmail.com>
2024-02-07 08:37:07 -0500

    Added an empty class Square that defines a square and README

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is a module which contains an empty class Square that defines a square.
+"""
+
+
+class Square:
+    """
+    This is a class which defines a square.
+    """
+    pass
