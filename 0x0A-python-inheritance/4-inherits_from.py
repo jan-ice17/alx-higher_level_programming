f17b8e4f4e34d3ba3f8e9276bf36520bf6a66dea
jan_ice17 <judysjewel17@gmail.com>
2024-02-21 09:18:05 -0500

    Remove trailing whitespace in 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,3 @@ The inherits_from function
 def inherits_from(obj, a_class):
     """It returns true if obj is a subclass of a_class, otherwise false"""
     return(issubclass(type(obj), a_class) and type(obj) != a_class)
- 
