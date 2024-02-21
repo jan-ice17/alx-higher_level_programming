bfcb8ed4866ae25395a1657b9c768e546cebd60b
jan_ice17 <judysjewel17@gmail.com>
2024-02-21 00:35:16 -0500

    ADded a class MyList that inherits from list:

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+MyList class function is used here
+"""
+class MyList(list):
+    """The subclass of list"""
+    def __init__(self):
+        """It initializes the object"""
+        super().__init__()
+
+    def print_sorted(self):
+        """It prints the sorted list"""
+        print(sorted(self))
+        
