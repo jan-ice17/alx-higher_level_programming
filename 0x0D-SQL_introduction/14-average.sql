7ce1faf69e2062e4edeeb654f44c85527d1c6f26
jan_ice17 <judysjewel17@gmail.com>
2024-03-28 18:04:20 -0400

    added 5 new tasks

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- This computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server
+-- THis computes an average of scores in a table
+SELECT AVG(`score`) 'average' FROM second_table;
