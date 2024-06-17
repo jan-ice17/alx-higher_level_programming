760ebcf1f1bd1e222f67795a4a3b2faaa3ee8dce
jan-ice17 <judysjewel17@gmail.com>
2024-06-17 23:44:41 +0300

    Added a script that searches the second biggest integer in the list of arguments.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,13 @@
 #!/usr/bin/node
 
-function largestint() {
-    
-    const args = process.argv.slice(2).map(Number);
+// Write a script that searches the second biggest integer in the list of arguments.
+// You can assume all arguments can be converted to integer
+// If no argument passed, print 0
+// If the number of arguments is 1, print 0
 
-    if (args.length <= 2) {
-        console.log('0')
-    }
-    else if (args.length == 3) {
-        console.log('0');
-    }
-    else {
-        console.log('0');
-    }
+if (process.argv.length <= 2) {
+    console.log(0);
+} else {
+    const s = process.argv.slice(2).map(Number).sort((x, y) => x - y);
+    console.log(s[s.length - 2]);
 }
-
-largestint();
