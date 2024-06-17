a293bbf99cfc3d372e7e4555fbe7646dc0a764f7
jan-ice17 <judysjewel17@gmail.com>
2024-06-18 00:28:27 +0300

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,18 +1,8 @@
 #!/usr/bin/node
+const argc = process.argv.length;
 
-// Write a script that prints a message depending of the number of arguments passed:
-// If no arguments are passed to the script, print “No argument”
-// If only one argument is passed to the script, print “Argument found”
-// Otherwise, print “Arguments found”
-// You must use console.log(...) to print all output
-// You are not allowed to use var Reference: process.argv
- 
-const argv = process.argv;
-
-if (argv.length === 2) {
-  console.log('No argument');  // no arg is found
-} else if (argv.length === 3) {
-  console.log('Argument found'); // only one arg
+if (argc > 2) {
+  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
 } else {
-  console.log('Arguments found');
+  console.log('No argument');
 }
