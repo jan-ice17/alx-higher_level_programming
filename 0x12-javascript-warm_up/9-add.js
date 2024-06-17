5c2cbcbb4a6e06d79b2d8f7632473627098bd64e
jan-ice17 <judysjewel17@gmail.com>
2024-06-17 21:59:04 +0300

    Added comments to file9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
+
+// Write a script that prints the addition of 2 integers
+
+// The first argument is the first integer
+// The second argument is the second integer
+// You have to define a function with this prototype: function add(a, b)
+
 const argv = process.argv;
 const add = (a, b) => a + b;
 
