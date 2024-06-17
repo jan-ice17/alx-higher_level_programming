7403529bd24e92064a90fe7c7ff67e21adeab3cb
jan-ice17 <judysjewel17@gmail.com>
2024-06-17 22:56:04 +0300

    fixed end of line issues

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,7 +5,7 @@
 // You must do it recursively
 
 function factorial (x) {
-    return x === 0 || isNaN(x) ? 1 : x * factorial(x - 1);
-  }
-  
-  console.log(factorial(Number(process.argv[2])));
+  return x === 0 || isNaN(x) ? 1 : x * factorial(x - 1);
+}
+
+console.log(factorial(Number(process.argv[2])));
