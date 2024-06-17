e34b984e12f92b2c049a3fddf3a7685e6a7f0591
jan-ice17 <judysjewel17@gmail.com>
2024-06-17 22:34:43 +0300

    Fixed semistandard issues

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,23 +4,8 @@
 // The first argument is integer (argument can be cast as integer)
 // You must do it recursively
 
-function factorial (n) {
-    if (n < 0) {
-      return 1;
-    } else if (n === 0) {
-      return 1;
-    } else {
-      return n * factorial(n - 1);
-    }
+function factorial (x) {
+    return x === 0 || isNaN(x) ? 1 : x * factorial(x - 1);
   }
   
-  const firstAgrs = process.argv[2];
-  const num = Number(firstAgrs);
-  
-  if (!Number.isInteger(num)) {
-    console.log(num);
-  } else {
-    const factorialResult = factorial(num);
-    console.log(factorialResult);
-  }
-  
+  console.log(factorial(Number(process.argv[2])));
