8d85f53099495378f451eb1b96dc762217438139
jan-ice17 <judysjewel17@gmail.com>
2024-06-17 19:53:15 +0300

    Add script that prints 3lines

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const multipleString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i <multipleString.length; i++) {
+    console.log(multipleString[i]);
+}
