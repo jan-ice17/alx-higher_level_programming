66a6242405012b43bc5b3582b997acba259583b6
jan-ice17 <judysjewel17@gmail.com>
2024-06-20 16:19:35 +0300

    Semistandard error fixed

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-
-// Write a script that prints “JavaScript is amazing”:
-// You must create a constant variable called myVar 
-// with the value “JavaScript is amazing”
-
+/*
+* Write a script that prints “JavaScript is amazing”
+* You must create a constant variable called myVar
+* with the value “JavaScript is amazing”
+*/
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
