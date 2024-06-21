43f05a48d6f354e4c14b61ef1a6748dcade9c795
jan_ice17 <judysjewel17@gmail.com>
2024-06-21 18:45:57 +0300

    Added main to the file

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
-""" Imported modules"""
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
+"""
 
 import MySQLdb
 import sys
 
-def database_hbtn():
-    db = MySQLdb.connect(
-        user=sys.argv[1], 
-        host=sys.argv[2], 
-        password='Password@5756',
-        database=sys.argv[3],
-        auth_plugin='mysql_native_password'
-        )
-    
-    mycursor = db.cursor()
-    return db, mycursor
+def hbtn_database
+myDb = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user_name=sys.argv[1],
+    my_password=sys.argv[2],
+    database=sys.argv[3]       
+    )
 
-#Task 0
-def database_show():
-    db, dbcursor = database_hbtn()
-    dbcursor.execute('USE hbtn_0e_0_usa')
-    dbcursor.execute('INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada")')
-    dbcursor.execute('SELECT * FROM states ORDER BY id ASC')
-    for i in dbcursor:
+MyCursor = myDb.cursor()
+return myDb, MyCursor
+
+
+#Solution to task 0
+def show_a_database():
+    myDb, Mycursor = hbtn_database()
+    Mycursor.execute('USE hbtn_0e_0_usa')
+    Mycursor.execute('INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada")')
+    Mycursor.execute('SELECT * FROM states ORDER BY id ASC')
+    for i in Mycursor:
         print(i)
-    dbcursor.close()
-    db.close()
+    Mycursor.close()
+    MyDb.close()
+
+def main():
+    show_a_database()
+   
+
+if __name__=='__main__':
+    main()
