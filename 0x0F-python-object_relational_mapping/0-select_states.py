c2940ced55c1b35e3d3543b868fe51c8f951c7c2
jan-ice17 <judysjewel17@gmail.com>
2024-06-21 15:17:00 +0300

    File 0 added

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Imported modules"""
+
+import MySQLdb
+import sys
+
+def hbtn_database
+myDb = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user_name=sys.argv[1],
+    my_password=sys.argv[2],
+    database=sys.argv[3]       
+    )
+
+MyCursor = myDb.cursor()
+return myDb, MyCursor
+
+
+#Solution to task 0
+def show_a_database():
+    myDb, Mycursor = hbtn_database()
+    Mycursor.execute('USE hbtn_0e_0_usa')
+    Mycursor.execute('INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada")')
+    Mycursor.execute('SELECT * FROM states ORDER BY id ASC')
+    for i in Mycursor:
+        print(i)
+    Mycursor.close()
+    MyDb.close()
