0f45c17a377062c3f944bc67eb411a678cbad5d5
jan_ice17 <judysjewel17@gmail.com>
2024-06-23 11:17:12 +0300

    0-select_states.py file added

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,39 +1,28 @@
 #!/usr/bin/python3
-"""
-This script lists all states from the
-database `hbtn_0e_0_usa`.
-"""
+"""This is a module to list all states from the database."""
 
 import MySQLdb
 import sys
 
-def hbtn_database
-myDb = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user_name=sys.argv[1],
-    my_password=sys.argv[2],
-    database=sys.argv[3]       
+
+def list_all_states():
+    """It connects to the database & gets all states."""
+    connection = MySQLdb.connect(
+        user=sys.argv[1],
+        host="localhost",
+        port=3306,
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    connection.close()
 
-MyCursor = myDb.cursor()
-return myDb, MyCursor
 
-
-#Solution to task 0
-def show_a_database():
-    myDb, Mycursor = hbtn_database()
-    Mycursor.execute('USE hbtn_0e_0_usa')
-    Mycursor.execute('INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada")')
-    Mycursor.execute('SELECT * FROM states ORDER BY id ASC')
-    for i in Mycursor:
-        print(i)
-    Mycursor.close()
-    MyDb.close()
-
-def main():
-    show_a_database()
-   
-
-if __name__=='__main__':
-    main()
+if __name__ == "__main__":
+    list_all_states()
+    
