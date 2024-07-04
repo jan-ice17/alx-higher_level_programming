35598a7e3e06b27b26ff413b4e16b06c2819a7d8
jan-ice17 <judysjewel17@gmail.com>
2024-07-04 08:07:39 -0700

    4-header.sh added

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL displays all HTTP methods the server will accept.
-curl -s-X GET "$1" -H "X-School-User-Id: 98"
+# script that takes in a URL displays all HTTP methopt.
+curl -s -X GET "$1" -H "X-School-User-Id: 98"
