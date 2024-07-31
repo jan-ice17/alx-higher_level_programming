1b3f664672c1511a90ec35ad3ba03f962dcf4a68
jan-ice17 <judysjewel17@gmail.com>
2024-08-01 02:38:44 +0300

    Files added

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movie_Id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movie_Id}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
+});
