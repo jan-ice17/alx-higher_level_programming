20be9841748724a6a03681e39d2a52685073506c
jan-ice17 <judysjewel17@gmail.com>
2024-08-01 03:13:34 +0300

    file 3 modified

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movie_Id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movie_Id}`;
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request.get(url, (error, response, body) => {
   if (error) {
